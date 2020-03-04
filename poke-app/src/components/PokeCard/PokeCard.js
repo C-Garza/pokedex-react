2ddfb3714de0f2360d8771479d7a65a90d47552f
Christian Garza <16683618+C-Garza@users.noreply.github.com>
2020-03-04 01:51:55 -0800

    Fetch pokemon data from pokeAPI

--- poke-app/src/components/PokeCard/PokeCard.js
+++ poke-app/src/components/PokeCard/PokeCard.js
@@ -1,23 +1,27 @@
 import React from "react";
 import styles from "./PokeCard.module.css";
 import pokeAPI from "../../apis/pokeAPI";
+import PokeHeader from "../PokeHeader/PokeHeader";
 
 class PokeCard extends React.Component {
-  state = {pokemon: {}};
+  state = {pokemon: null};
   async componentDidMount() {
     const response = await pokeAPI.get(`pokemon/${this.props.pokemon.name}`);
     // console.log(response.data);
     this.setState({pokemon: response.data});
   }
   render() {
-    console.log(this.state.pokemon);
+    if(!this.state.pokemon) {
+      return <div>Loading...</div>;
+    }
     return(
       <div>
-        {this.props.pokemon.name}
+        <PokeHeader id={this.state.pokemon.id} name={this.state.pokemon.name} />
+        {/* {this.props.pokemon.name}
         <br />
         {this.state.pokemon.weight / 10}kg
         <br />
-        {this.state.pokemon.height / 10}m
+        {this.state.pokemon.height / 10}m */}
       </div>
     );
   }
