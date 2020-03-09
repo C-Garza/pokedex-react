3ba269adf790d799227df3367e27fa07f7d3fa10
Christian Garza <16683618+C-Garza@users.noreply.github.com>
2020-03-09 16:00:54 -0700

    Add PokePage route

--- poke-app/src/components/App.js
+++ poke-app/src/components/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter, Route, Switch} from "react-router-dom";
 import styles from "../App.module.css";
 import Header from "./Header/Header";
 import PokeListContainer from "./PokeListContainer/PokeListContainer";
+import PokePageContainer from "./PokePageContainer/PokePageContainer";
 
 class App extends React.Component {
   render() {
@@ -12,6 +13,7 @@ class App extends React.Component {
           <Header />
           <Switch>
             <Route path="/" exact component={PokeListContainer} />
+            <Route path="/pokemon/:id" exact component={PokePageContainer} />
           </Switch>
         </BrowserRouter>
       </div>
