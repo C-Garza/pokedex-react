8f184243e21f7dcaae974e1336a7939a510b54d1
Christian Garza <16683618+C-Garza@users.noreply.github.com>
2020-03-09 15:59:45 -0700

    Add parmas to fetchPokeList

--- poke-app/src/actions/index.js
+++ poke-app/src/actions/index.js
@@ -4,8 +4,11 @@ import {
   FETCH_POKEMON
 } from "./types";
 
-export const fetchPokeList = () => async dispatch => {
-  const response = await pokeAPI.get("pokemon");
+export const fetchPokeList = (limit, offset) => async dispatch => {
+  const response = await pokeAPI.get("pokemon", {params: {
+    limit: limit,
+    offset: offset
+  }});
   console.log(response.data.results);
   dispatch({type: FETCH_POKE_LIST, payload: response.data.results});
 };
