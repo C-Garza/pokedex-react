99796481d624995adde50df59e9bf98c042c4169
Christian Garza <16683618+C-Garza@users.noreply.github.com>
2020-03-08 22:53:39 -0700

    Add react-router-dom links and header

--- poke-app/src/components/App.js
+++ poke-app/src/components/App.js
@@ -1,14 +1,19 @@
 import React from "react";
-import PokeCard from "./PokeCard/PokeCard";
-import pokeAPI from "../apis/pokeAPI";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
 import styles from "../App.module.css";
+import Header from "./Header/Header";
 import PokeListContainer from "./PokeListContainer/PokeListContainer";
 
 class App extends React.Component {
   render() {
     return(
       <div>
-        <PokeListContainer />
+        <BrowserRouter>
+          <Header />
+          <Switch>
+            <Route path="/" exact component={PokeListContainer} />
+          </Switch>
+        </BrowserRouter>
       </div>
     );
   }
