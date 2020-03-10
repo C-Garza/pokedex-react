4a1b5f27c35992696bf52b5c6e9922b1c4cb699f
Christian Garza <16683618+C-Garza@users.noreply.github.com>
2020-03-10 02:02:32 -0700

    Add styles for extended version of card

--- poke-app/src/components/PokeHeader/PokeHeader.js
+++ poke-app/src/components/PokeHeader/PokeHeader.js
@@ -1,11 +1,11 @@
 import React from "react";
 import styles from "./PokeHeader.module.css";
 
-const PokeHeader = ({name = "Pokemon", id, typeClass = "grass"}) => {
+const PokeHeader = ({name = "Pokemon", id, typeClass = "grass", extended = false}) => {
   return(
-    <div className={`${styles.header} ${typeClass.border}`}>
+    <div className={extended ? `${styles.header__ext} ${typeClass.border}` : `${styles.header} ${typeClass.border}`}>
       <img 
-        className={`${styles.header__image} img-hover`}
+        className={extended ? `${styles.header__ext__image} img-hover` : `${styles.header__image} img-hover`}
         src={id ? `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png` : ""}
         alt={name}
         data-hoverable="true"
