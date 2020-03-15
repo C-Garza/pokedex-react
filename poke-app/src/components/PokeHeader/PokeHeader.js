7e6d8e0a2bc9272bc7ed3365bbd288d4298ed19a
Christian Garza <16683618+C-Garza@users.noreply.github.com>
2020-03-14 17:03:07 -0700

    Add id number and style it

--- poke-app/src/components/PokeHeader/PokeHeader.js
+++ poke-app/src/components/PokeHeader/PokeHeader.js
@@ -1,8 +1,18 @@
 import React from "react";
 import styles from "./PokeHeader.module.css";
 
-const PokeHeader = ({name = "Pokemon", id, typeClass = "grass", extended = false}) => {
+const PokeHeader = ({name = "Pokemon", id=null, typeClass = "grass", extended = false}) => {
+  const getPokeNumber = () => {
+    if(id < 10) {
+      id = `00${id}`;
+    }
+    else if(id < 100) {
+      id = `0${id}`;
+    }
+    return id;
+  };
   return(
+    <React.Fragment>
     <div className={extended ? `${styles.header__ext} ${typeClass.border}` : `${styles.header} ${typeClass.border}`}>
       <img 
         className={extended ? `${styles.header__ext__image} img-hover` : `${styles.header__image} img-hover`}
@@ -11,6 +21,8 @@ const PokeHeader = ({name = "Pokemon", id, typeClass = "grass", extended = false
         data-hoverable="true"
       />
     </div>
+    <div className={extended ? `${styles.header__ext__id}` : `${styles.header__id}`}><span>#</span>{getPokeNumber()}</div>
+    </React.Fragment>
   );
 };
 
