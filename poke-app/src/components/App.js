328f96463e88058ae20aea7ee6818688cc5ae52a
Christian Garza <16683618+C-Garza@users.noreply.github.com>
2020-06-02 16:21:13 -0700

    Change route links

--- poke-app/src/components/App.js
+++ poke-app/src/components/App.js
@@ -2,8 +2,9 @@ import React from "react";
 import {BrowserRouter, Route, Switch} from "react-router-dom";
 import styles from "../App.module.css";
 import Header from "./Header/Header";
-import PokeListContainer from "./PokeListContainer/PokeListContainer";
+import HomePage from "./HomePage/HomePage";
 import PokePageContainer from "./PokePageContainer/PokePageContainer";
+import SearchResultsPage from "./SearchResultsPage/SearchResultsPage";
 
 class App extends React.Component {
   render() {
@@ -12,8 +13,9 @@ class App extends React.Component {
         <BrowserRouter>
           <Header />
           <Switch>
-            <Route path="/" exact component={PokeListContainer} />
+            <Route path="/" exact component={HomePage} />
             <Route path="/pokemon/:id" exact component={PokePageContainer} />
+            <Route path="/search" exact component={SearchResultsPage} />
           </Switch>
         </BrowserRouter>
       </div>
