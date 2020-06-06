a1ff89f5db69e608c991e27305b6a0f4f9ac904a
Christian Garza <16683618+C-Garza@users.noreply.github.com>
2020-06-05 23:10:25 -0700

    Add state to pass offset to PokeLIstContainer and render PokeListContainer

--- poke-app/src/components/HomePage/HomePage.js
+++ poke-app/src/components/HomePage/HomePage.js
@@ -3,9 +3,17 @@ import styles from "./HomePage.module.css";
 import PokeListContainer from "../PokeListContainer/PokeListContainer";
 
 class HomePage extends React.Component {
+  state = {offset: 0};
+
+  componentDidMount() {
+    this.handleUpdateOffset(24);
+  }
+  handleUpdateOffset = (offset) => {
+    this.setState({offset: offset});
+  }
   render() {
     return(
-      <PokeListContainer />
+      <PokeListContainer handleUpdateOffset={this.handleUpdateOffset} offset={this.state.offset} />
     );
   }
 }
