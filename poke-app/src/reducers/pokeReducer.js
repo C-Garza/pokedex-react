2b964706f0f92330eb36f654293bc0cd9801d43d
Christian Garza <16683618+C-Garza@users.noreply.github.com>
2020-06-05 23:09:38 -0700

    Add FETCH_TYPE case to add types to store

--- poke-app/src/reducers/pokeReducer.js
+++ poke-app/src/reducers/pokeReducer.js
@@ -44,11 +44,20 @@ export default (state=INIT_STATE, action) => {
         }
       };
     case FETCH_TYPE:
+      ////FILTER OUT POKE FORMS AND
+      ////RETURN SINGLE OBJECT WITH POKEMON AND SLOT NUM
+      let pokeList = action.payload.pokemon.filter(poke => {
+        ////TO-DO: CHANGE 807 TO state.pokeList WITHOUT RACE CONDITION
+        return parseInt(poke.pokemon.url.slice(34, -1), 10) <= 807;
+      }).map(poke => {
+        poke.pokemon.slot = poke.slot;
+        return poke.pokemon;
+      });
       return {
         ...state,
         types: {
           ...state.types,
-          [action.payload.name]: [...action.payload.pokemon]
+          [action.payload.name]: [...pokeList]
         }
       };
     default:
