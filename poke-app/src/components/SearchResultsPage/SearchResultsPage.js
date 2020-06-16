b279aaf6731824765cbf1ef8479a028b2acbcf00
Christian Garza <16683618+C-Garza@users.noreply.github.com>
2020-06-16 01:13:52 -0700

    Change filteredType state name and add filterByName handler and renderHeading function. Fix no query type existing throwing error

--- poke-app/src/components/SearchResultsPage/SearchResultsPage.js
+++ poke-app/src/components/SearchResultsPage/SearchResultsPage.js
@@ -8,7 +8,7 @@ import {fetchTypes} from "../../actions";
 class SearchResultsPage extends React.Component {
   ////HANDLE CONDITION THAT SEARCH RETURNS NOTHING(i.e. FAIRY/DARK)
   ////CHECK IN RENDER IF TYPE IS SAME AS PREVIOUS ONE (NEW STATES TO HOLD)
-  state = {filteredTypes: [], offset: 0};
+  state = {filteredPoke: [], offset: 0};
 
   componentDidMount() {
     const NUM_OF_TYPES = 19;
@@ -35,12 +35,42 @@ class SearchResultsPage extends React.Component {
     else if(query.type) {
       this.filterByType(query.type);
     }
+    else if(query.name) {
+      this.filterByName(query.name);
+    }
   }
   handleUpdateOffset = (offset) => {
     this.setState({offset: offset});
   }
+  filterByName = (name) => {
+    const {pokeList} = this.props;
+    this.handleUpdateOffset(24);
+    console.log(name);
+    let filteredSuggestions = pokeList.filter(suggestion => {
+      return suggestion.name.toLowerCase().indexOf(name.toLowerCase()) > -1;
+    }).sort((a,b) => {
+      a = a.name.toLowerCase();
+      b = b.name.toLowerCase();
+      return a.indexOf(name) - b.indexOf(name);
+    });
+    console.log(filteredSuggestions);
+    this.setState({filteredPoke: filteredSuggestions});
+  }
   filterByType = (types) => {
     const {pokemonTypes} = this.props;
+    ////CHECK IF QUERY EXISTS IN STATE
+    if(Array.isArray(types)) {
+      if(!types.every(type => {
+        return pokemonTypes[type];
+      })) {
+        return;
+      }
+    }
+    else {
+      if(!pokemonTypes[types]) {
+        return;
+      }
+    }
     this.handleUpdateOffset(24);
     if(Array.isArray(types)) { ////EACH POKE MUST CONTAIN THESE TYPES(NO-ORDER, CAN'T HAVE 3 TYPED POKEMON)
       let typesArrOne = new Set(pokemonTypes[types[0]].map(poke => poke.name));
@@ -61,26 +91,50 @@ class SearchResultsPage extends React.Component {
         }
       })];
       console.log("FILTERED POKE: ", mergedTypesArr);
-      this.setState({filteredTypes: mergedTypesArr});
+      this.setState({filteredPoke: mergedTypesArr});
     }
     else { ////SEARCH OF ONE TYPE
       console.log("FILTERED POKE: ", pokemonTypes[types]);
-      this.setState({filteredTypes: pokemonTypes[types]});
+      this.setState({filteredPoke: pokemonTypes[types]});
+    }
+  }
+  renderHeading = (query) => {
+    const {filteredPoke} = this.state;
+    if(!Object.keys(query).length) {
+      return;
+    }
+    else if(query.type) {
+      let typeList = Array.isArray(query.type) ? query.type.join(" & ") : query.type;
+      return(
+        <h1 className={styles.search__results__header}>Showing {filteredPoke.length} result(s) for: {typeList}</h1>
+      );
+    }
+    else if(query.name) {
+      return(
+        <h1 className={styles.search__results__header}>Showing {filteredPoke.length} result(s) for: {query.name}</h1>
+      );
     }
   }
   render() {
+    let query = queryString.parse(this.props.location.search);
     return(
-      <PokeListContainer 
-        searchResults={this.state.filteredTypes} 
-        handleUpdateOffset={this.handleUpdateOffset} 
-        offset={this.state.offset}
-      />
+      <div className={styles.search__results__container}>
+        <div className={styles.search__results__header__container}>
+          {this.renderHeading(query)}
+        </div>
+        <PokeListContainer 
+          searchResults={this.state.filteredPoke} 
+          handleUpdateOffset={this.handleUpdateOffset} 
+          offset={this.state.offset}
+        />
+      </div>
     );
   }
 }
 
 const mapStateToProps = (state) => {
   return {
+    pokeList: state.pokemon.pokemonAll.pokeList,
     pokemonTypes: state.pokemon.types
   };
 };
