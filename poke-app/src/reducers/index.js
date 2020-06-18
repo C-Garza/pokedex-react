538a28e60d3290f747e9c1f7164e5ed67432caf7
Christian Garza <16683618+C-Garza@users.noreply.github.com>
2020-06-18 03:09:32 -0700

    Add userPreferences.js reducer

--- poke-app/src/reducers/index.js
+++ poke-app/src/reducers/index.js
@@ -1,6 +1,8 @@
 import {combineReducers} from "redux";
 import pokeReducer from "./pokeReducer";
+import userPreferences from "./userPreferences";
 
 export default combineReducers({
-  pokemon: pokeReducer
+  pokemon: pokeReducer,
+  userPreferences: userPreferences
 });
