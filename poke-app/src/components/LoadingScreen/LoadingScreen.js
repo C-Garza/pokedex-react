8af03cfe328547b38807dbae235e2e1181d30d65
Christian Garza <16683618+C-Garza@users.noreply.github.com>
2020-07-04 17:51:09 -0700

    Add pokeball loading screen and styles and animation

--- poke-app/src/components/LoadingScreen/LoadingScreen.js
+++ poke-app/src/components/LoadingScreen/LoadingScreen.js
@@ -3,7 +3,19 @@ import styles from "./LoadingScreen.module.css";
 
 const LoadingScreen = () => {
   return(
-    <div>Loading...</div>
+    <React.Fragment>
+      <div className={styles.pokeball__container}>
+        <div className={styles.pokeball}>
+          <div className={styles.pokeball__center}>
+            <div className={styles.pokeball__center__light}></div>
+            <div className={styles.pokeball__center__light__on}></div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.heading__container}>
+        <h2 className={styles.heading__header}>Loading...</h2>
+      </div>
+    </React.Fragment>
   );
 };
 
