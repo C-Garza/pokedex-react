74060570017a394943b18366d8a97dd5744f50c1
Christian Garza <16683618+C-Garza@users.noreply.github.com>
2020-07-13 20:27:18 -0700

    Add axios-retry and delay

--- poke-app/src/apis/pokeAPI.js
+++ poke-app/src/apis/pokeAPI.js
@@ -1,8 +1,15 @@
 import axios from "axios";
+import axiosRetry from "axios-retry";
 
-export default axios.create({
+
+export const pokeAPI = axios.create({
   baseURL: "https://pokeapi.co/api/v2/",
   params: {
     limit: 6
   }
+});
+
+axiosRetry(pokeAPI, {
+  retries: 5,
+  retryDelay: axiosRetry.exponentialDelay
 });
