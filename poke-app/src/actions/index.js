5c1d6753e6e1f6746e9acab273d99c5ee9a4208d
Christian Garza <16683618+C-Garza@users.noreply.github.com>
2020-07-13 20:25:04 -0700

    Add error handling actions and reducers

--- poke-app/src/actions/index.js
+++ poke-app/src/actions/index.js
@@ -1,50 +1,131 @@
-import pokeAPI from "../apis/pokeAPI";
+import {pokeAPI} from "../apis/pokeAPI";
 import {
   FETCH_POKE_LIST,
+  FETCH_POKE_LIST_ERROR,
   FETCH_POKEMON,
+  FETCH_POKEMON_ERROR,
   FETCH_POKEMON_SPECIES,
+  FETCH_POKEMON_SPECIES_ERROR,
   FETCH_TYPE,
+  FETCH_TYPE_ERROR,
   FETCH_EVOLUTION_CHAIN,
+  FETCH_EVOLUTION_CHAIN_ERROR,
   SET_CHART_PREFERENCE
 } from "./types";
 
+const handleError = (err, fn, dispatch, data) => {
+  if(err.response) {
+    console.log(err.response.data);
+    console.log(err.response.status);
+    console.log(err.response.headers);
+    if(data) {
+      dispatch(fn(err, data));
+    }
+    else {
+      dispatch(fn(err));
+    }
+  }
+  if(err.request) {
+    console.log(err.request);
+    if(data) {
+      dispatch(fn(err, data));
+    }
+    else {
+      dispatch(fn(err));
+    }
+  }
+  else {
+    console.log("Error: ", err.message);
+    if(data) {
+      dispatch(fn(err, data));
+    }
+    else {
+      dispatch(fn(err));
+    }
+  }
+};
+
 export const fetchPokeList = (limit, offset) => async dispatch => {
-  const response = await pokeAPI.get("pokemon", {params: {
-    limit: limit,
-    offset: offset
-  }});
-  console.log(response.data.results);
-  dispatch({type: FETCH_POKE_LIST, payload: response.data.results});
-  return Promise.resolve({next: response.data.next, prev: response.data.previous});
+  try {
+    const response = await pokeAPI.get("pokemon", {params: {
+      limit: limit,
+      offset: offset
+    }});
+    console.log(response.data.results);
+    dispatch({type: FETCH_POKE_LIST, payload: response.data.results});
+    return Promise.resolve({next: response.data.next, prev: response.data.previous});
+  } catch (err) {
+    handleError(err, fetchPokeListError, dispatch);
+    return Promise.reject(err);
+  }
+};
+
+export const fetchPokeListError = (err) => {
+  return {type: FETCH_POKE_LIST_ERROR, payload: err};
 };
 
 export const fetchPokemon = (name, isExtended = false) => async dispatch => {
-  const response = await pokeAPI.get(`pokemon/${name}`);
-  console.log(response.data);
-  dispatch({type: FETCH_POKEMON, payload: response.data});
-  if(isExtended) {
-    return dispatch(fetchPokemonSpecies(response.data.id));
+  try {
+    const response = await pokeAPI.get(`pokemon/${name}`);
+    console.log(response.data);
+    dispatch({type: FETCH_POKEMON, payload: response.data});
+    if(isExtended) {
+      return dispatch(fetchPokemonSpecies(response.data.id, response.data.name));
+    }
+  } catch (err) {
+    handleError(err, fetchPokemonError, dispatch, name);
+    return Promise.reject(err);
   }
-  return Promise.resolve(true);
 };
 
-export const fetchPokemonSpecies = (name) => async dispatch => {
-  const response = await pokeAPI.get(`pokemon-species/${name}`);
-  console.log(response.data);
-  dispatch({type: FETCH_POKEMON_SPECIES, payload: response.data});
+export const fetchPokemonError = (err, name) => {
+  return {type: FETCH_POKEMON_ERROR, payload: {err: err, name: name}};
+};
+
+export const fetchPokemonSpecies = (id, name) => async dispatch => {
+  try {
+    const response = await pokeAPI.get(`pokemon-species/${id}`);
+    console.log(response.data);
+    dispatch({type: FETCH_POKEMON_SPECIES, payload: response.data});
+  } catch (err) {
+    handleError(err, fetchPokemonSpeciesError, dispatch, name);
+    return Promise.reject(err);
+  }
+};
+
+export const fetchPokemonSpeciesError = (err, name) => {
+  return {type: FETCH_POKEMON_SPECIES_ERROR, payload: {err: err, name: name}};
 };
 
 export const fetchTypes = (index) => async dispatch => {
-  const response = await pokeAPI.get(`type/${index}`);
-  console.log(response.data);
-  dispatch({type: FETCH_TYPE, payload: response.data});
+  try {
+    const response = await pokeAPI.get(`type/${index}`);
+    console.log(response.data);
+    dispatch({type: FETCH_TYPE, payload: response.data});
+  } catch (err) {
+    handleError(err, fetchTypesError, dispatch, index);
+    return Promise.reject(err);
+  }
+};
+
+export const fetchTypesError = (err, id) => {
+  return {type: FETCH_TYPE_ERROR, payload: {err: err, id: id}};
 };
 
 export const fetchEvolutionChain = (index) => async dispatch => {
-  const response = await pokeAPI.get(`evolution-chain/${index}`);
-  console.log(response.data);
-  dispatch({type: FETCH_EVOLUTION_CHAIN, payload: response.data});
-  return Promise.resolve(true);
+  try {
+    const response = await pokeAPI.get(`evolution-chain/${index}`);
+    console.log(response.data);
+    dispatch({type: FETCH_EVOLUTION_CHAIN, payload: response.data});
+    return Promise.resolve(true);
+  } catch (err) {
+    handleError(err, fetchEvolutionChainError, dispatch, index);
+    return Promise.reject(err);
+  }
+};
+
+export const fetchEvolutionChainError = (err, index) => {
+  return {type: FETCH_EVOLUTION_CHAIN_ERROR, payload: {err: err, id: index}};
 };
 
 export const setChartPreference = (chart) => {
--- poke-app/src/reducers/pokeReducer.js
+++ poke-app/src/reducers/pokeReducer.js
@@ -1,23 +1,29 @@
 import {
   FETCH_POKE_LIST, 
   FETCH_POKEMON,
+  FETCH_POKEMON_ERROR,
   FETCH_POKEMON_SPECIES,
   FETCH_TYPE,
   FETCH_EVOLUTION_CHAIN,
+  FETCH_POKE_LIST_ERROR,
+  FETCH_POKEMON_SPECIES_ERROR,
+  FETCH_EVOLUTION_CHAIN_ERROR,
+  FETCH_TYPE_ERROR,
 } from "../actions/types";
 
 const INIT_STATE = {
   pokemonAll: {
-    pokeList: []
+    pokeList: [],
+    error: null
   },
   pokemonObjs: {
-
+    error: null
   },
   evolution_chains: {
-
+    error: null
   },
   types: {
-
+    error: []
   }
 };
 
@@ -26,16 +32,37 @@ export default (state=INIT_STATE, action) => {
     case FETCH_POKE_LIST:
       return {
         ...state,
-        pokemonAll: {...state.pokemonAll, pokeList: [...state.pokemonAll.pokeList, ...action.payload]}
+        pokemonAll: {
+          ...state.pokemonAll, 
+          pokeList: [...action.payload],
+          error: null
+        },
+      };
+    case FETCH_POKE_LIST_ERROR:
+      return {
+        ...state,
+        pokemonAll: {
+          ...state.pokemonAll,
+          error: action.payload
+        }
       };
     case FETCH_POKEMON:
       return {
         ...state,
         pokemonObjs: {
           ...state.pokemonObjs,
-          [action.payload.name]: action.payload
+          [action.payload.name]: action.payload,
+          error: null
         }
       };
+    case FETCH_POKEMON_ERROR:
+      return {
+        ...state,
+        pokemonObjs: {
+          ...state.pokemonObjs,
+          error: action.payload
+        }
+      }
     case FETCH_POKEMON_SPECIES:
       return {
         ...state,
@@ -43,10 +70,22 @@ export default (state=INIT_STATE, action) => {
           ...state.pokemonObjs,
           [action.payload.varieties[0].pokemon.name]: {
             ...state.pokemonObjs[action.payload.varieties[0].pokemon.name],
+            error: null,
             species_ext: action.payload
           }
         }
       };
+    case FETCH_POKEMON_SPECIES_ERROR:
+      return {
+        ...state,
+        pokemonObjs: {
+          ...state.pokemonObjs,
+          [action.payload.name]: {
+            ...state.pokemonObjs[action.payload.name],
+            error: action.payload.err
+          }
+        }
+      }
     case FETCH_TYPE:
       ////FILTER OUT POKE FORMS AND
       ////RETURN SINGLE OBJECT WITH POKEMON AND SLOT NUM
@@ -57,18 +96,51 @@ export default (state=INIT_STATE, action) => {
         poke.pokemon.slot = poke.slot;
         return poke.pokemon;
       });
+      let errors = [];
+      if(state.types.error.length > 0) {
+        errors = state.types.error.filter(type => {
+          return type.id !== action.payload.id;
+        });
+      }
       return {
         ...state,
         types: {
           ...state.types,
-          [action.payload.name]: [...pokeList]
+          [action.payload.name]: [...pokeList],
+          error: errors
         }
       };
+    case FETCH_TYPE_ERROR:
+      let hasError = -1;
+      if(state.types.error.length) {
+        hasError = state.types.error.findIndex(type => type.id === action.payload.id);
+      }
+      if(hasError !== -1) {
+        return {
+          ...state,
+          types: {
+            ...state.types,
+            error: [...state.types.error]
+          }
+        }
+      }
+      return {
+        ...state,
+        types: {
+          ...state.types,
+          error: [...state.types.error, {err: action.payload.err, id: action.payload.id}]
+        }
+      }
     case FETCH_EVOLUTION_CHAIN:
       return {
         ...state,
-        evolution_chains: {...state.evolution_chains, [action.payload.id]: action.payload}
+        evolution_chains: {...state.evolution_chains, [action.payload.id]: action.payload, error: null}
       };
+    case FETCH_EVOLUTION_CHAIN_ERROR:
+      return {
+        ...state,
+        evolution_chains: {...state.evolution_chains, error: action.payload}
+      }
     default:
       return state;
   }
