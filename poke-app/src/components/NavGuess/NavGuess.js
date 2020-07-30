d19821a9f74ac61097eddb0bbedb9779c4d3731e
Christian Garza <16683618+C-Garza@users.noreply.github.com>
2020-07-30 03:05:20 -0700

    Add NavGuess

---
+++ poke-app/src/components/NavGuess/NavGuess.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./NavGuess.module.css";
+
+class NavGuess extends React.Component {
+  render() {
+    return(
+      <div className={styles.guess__container}>
+        
+      </div>
+    )
+  }
+}
+
+export default NavGuess;
