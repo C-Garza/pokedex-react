fadc1d5afdea8ee1c72eb7a2484fa2ac7d9e1d1b
Christian Garza <16683618+C-Garza@users.noreply.github.com>
2020-08-11 01:19:06 -0700

    Add NavAbout component

---
+++ poke-app/src/components/NavAbout/NavAbout.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./NavAbout.module.css";
+
+class NavAbout extends React.Component {
+  render() {
+    return(
+      <div>About</div>
+    );
+  }
+}
+
+export default NavAbout;
