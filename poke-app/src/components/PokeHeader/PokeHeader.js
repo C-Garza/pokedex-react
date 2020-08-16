6ba81f51e9f2003a05abe65b4d69735645f9c38e
Christian Garza <16683618+C-Garza@users.noreply.github.com>
2020-08-16 14:48:13 -0700

    Add markup for gif images and get images in new getGif and getPng methods.

--- poke-app/src/components/PokeHeader/PokeHeader.js
+++ poke-app/src/components/PokeHeader/PokeHeader.js
@@ -1,7 +1,14 @@
-import React from "react";
+import React, {useState, useEffect, useRef} from "react";
 import styles from "./PokeHeader.module.css";
 
-const PokeHeader = ({name = "Pokemon", id=null, typeClass = "grass", extended = false}) => {
+const PokeHeader = ({name = "Pokemon", id=null, typeClass = "grass", extended = false, hasEntered, width}) => {
+  const [height, setHeight] = useState(0);
+  const imgContainerRef = useRef(null);
+
+  useEffect(() => {
+    setHeight(imgContainerRef.current.clientHeight - 14);
+  }, [height, width]);
+
   const getPokeNumber = () => {
     if(id < 10) {
       id = `00${id}`;
@@ -11,15 +18,42 @@ const PokeHeader = ({name = "Pokemon", id=null, typeClass = "grass", extended = 
     }
     return id;
   };
+  const getScale = (e) =>  {
+    return {transform: `scale(${height / 90})`};
+  };
+  const getGif = (e) => {
+    return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/${id}.gif`
+  };
+  const getPng = (e) => {
+    return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`;
+  }
   return(
     <React.Fragment>
-    <div className={extended ? `${styles.header__ext}` : `${styles.header} ${typeClass.border}`}>
+    <div className={extended ? `${styles.header__ext}` : `${styles.header} ${typeClass.border}`} ref={imgContainerRef}>
+      <div 
+        className={`${styles.img__container} ${hasEntered ? `${styles.hasEntered}` : `${styles.hasLeft}`}`}
+        style={hasEntered ? getScale() : {}}
+      >
       <img 
-        className={extended ? `${styles.header__ext__image} img-hover` : `${styles.header__image} img-hover`}
-        src={id ? `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png` : ""}
+        className={
+          `${extended ? `${styles.header__ext__image} img-hover` : `${styles.header__image} img-hover`} 
+          ${hasEntered ? styles.hidden : ""}`
+        }
+        src={id ? getPng() : ""}
         alt={name}
         data-hoverable="true"
       />
+      <img 
+        className={
+          `${extended ? `${styles.header__ext__image} img-hover` : `${styles.header__image} img-hover`} 
+          ${hasEntered ? styles.img__gif : styles.img__gif__hidden}`
+        }
+        src={id ? hasEntered ? getGif() : getPng() : ""}
+        alt={name}
+        data-hoverable="true"
+        style={extended ? {display: "none"} : {}}
+      />
+      </div>
     </div>
     <div className={extended ? `${styles.header__ext__id}` : `${styles.header__id}`}><span>#</span>{getPokeNumber()}</div>
     </React.Fragment>
