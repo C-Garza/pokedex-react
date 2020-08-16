5f05e51f28f7a5b6134e79327a2b61b08a0d1504
Christian Garza <16683618+C-Garza@users.noreply.github.com>
2020-08-16 14:40:18 -0700

    Add gradCard property to getTypesClass

--- poke-app/src/components/utils/helper-functions.js
+++ poke-app/src/components/utils/helper-functions.js
@@ -7,126 +7,144 @@ export const getTypesClass = (types) => {
       return {
         card: `${typesClass.pokeTypeLogoFireCard}`, 
         logo: `${typesClass.pokeTypeLogoFire}`, 
-        border: `${typesClass.pokeTypeLogoFireCardBorder}`
+        border: `${typesClass.pokeTypeLogoFireCardBorder}`,
+        gradCard: `${typesClass.pokeTypeLogoGradFireCard}`
       }
     }
     else if(type === "water") {
       return {
         card: `${typesClass.pokeTypeLogoWaterCard}`, 
         logo: `${typesClass.pokeTypeLogoWater}`,
-        border: `${typesClass.pokeTypeLogoWaterCardBorder}`
+        border: `${typesClass.pokeTypeLogoWaterCardBorder}`,
+        gradCard: `${typesClass.pokeTypeLogoGradWaterCard}`
       };
     }
     else if(type === "grass") {
       return {
         card: `${typesClass.pokeTypeLogoGrassCard}`, 
         logo: `${typesClass.pokeTypeLogoGrass}`,
-        border: `${typesClass.pokeTypeLogoGrassCardBorder}`
+        border: `${typesClass.pokeTypeLogoGrassCardBorder}`,
+        gradCard: `${typesClass.pokeTypeLogoGradGrassCard}`
       };
     }
     else if(type === "poison") {
       return {
         card: `${typesClass.pokeTypeLogoPoisonCard}`, 
         logo: `${typesClass.pokeTypeLogoPoison}`,
-        border: `${typesClass.pokeTypeLogoPoisonCardBorder}`
+        border: `${typesClass.pokeTypeLogoPoisonCardBorder}`,
+        gradCard: `${typesClass.pokeTypeLogoGradPoisonCard}`
       };
     }
     else if(type === "bug") {
       return {
         card: `${typesClass.pokeTypeLogoBugCard}`, 
         logo: `${typesClass.pokeTypeLogoBug}`,
-        border: `${typesClass.pokeTypeLogoBugCardBorder}`
+        border: `${typesClass.pokeTypeLogoBugCardBorder}`,
+        gradCard: `${typesClass.pokeTypeLogoGradBugCard}`
       };
     }
     else if(type === "normal") {
       return {
         card: `${typesClass.pokeTypeLogoNormalCard}`, 
         logo: `${typesClass.pokeTypeLogoNormal}`,
-        border: `${typesClass.pokeTypeLogoNormalCardBorder}`
+        border: `${typesClass.pokeTypeLogoNormalCardBorder}`,
+        gradCard: `${typesClass.pokeTypeLogoGradNormalCard}`
       };
     }
     else if(type === "flying") {
       return {
         card: `${typesClass.pokeTypeLogoFlyingCard}`, 
         logo: `${typesClass.pokeTypeLogoFlying}`,
-        border: `${typesClass.pokeTypeLogoFlyingCardBorder}`
+        border: `${typesClass.pokeTypeLogoFlyingCardBorder}`,
+        gradCard: `${typesClass.pokeTypeLogoGradFlyingCard}`
       };
     }
     else if(type === "electric") {
       return {
         card: `${typesClass.pokeTypeLogoElectricCard}`, 
         logo: `${typesClass.pokeTypeLogoElectric}`,
-        border: `${typesClass.pokeTypeLogoElectricCardBorder}`
+        border: `${typesClass.pokeTypeLogoElectricCardBorder}`,
+        gradCard: `${typesClass.pokeTypeLogoGradElectricCard}`
       };
     }
     else if(type === "ground") {
       return {
         card: `${typesClass.pokeTypeLogoGroundCard}`, 
         logo: `${typesClass.pokeTypeLogoGround}`,
-        border: `${typesClass.pokeTypeLogoGroundCardBorder}`
+        border: `${typesClass.pokeTypeLogoGroundCardBorder}`,
+        gradCard: `${typesClass.pokeTypeLogoGradGroundCard}`
       };
     }
     else if(type === "fairy") {
       return {
         card: `${typesClass.pokeTypeLogoFairyCard}`, 
         logo: `${typesClass.pokeTypeLogoFairy}`,
-        border: `${typesClass.pokeTypeLogoFairyCardBorder}`
+        border: `${typesClass.pokeTypeLogoFairyCardBorder}`,
+        gradCard: `${typesClass.pokeTypeLogoGradFairyCard}`
       };
     }
     else if(type === "fighting") {
       return {
         card: `${typesClass.pokeTypeLogoFightingCard}`, 
         logo: `${typesClass.pokeTypeLogoFighting}`,
-        border: `${typesClass.pokeTypeLogoFightingCardBorder}`
+        border: `${typesClass.pokeTypeLogoFightingCardBorder}`,
+        gradCard: `${typesClass.pokeTypeLogoGradFightingCard}`
       };
     }
     else if(type === "psychic") {
       return {
         card: `${typesClass.pokeTypeLogoPsychicCard}`, 
         logo: `${typesClass.pokeTypeLogoPsychic}`,
-        border: `${typesClass.pokeTypeLogoPsychicCardBorder}`
+        border: `${typesClass.pokeTypeLogoPsychicCardBorder}`,
+        gradCard: `${typesClass.pokeTypeLogoGradPsychicCard}`
       };
     }
     else if(type === "rock") {
       return {
         card: `${typesClass.pokeTypeLogoRockCard}`, 
         logo: `${typesClass.pokeTypeLogoRock}`,
-        border: `${typesClass.pokeTypeLogoRockCardBorder}`
+        border: `${typesClass.pokeTypeLogoRockCardBorder}`,
+        gradCard: `${typesClass.pokeTypeLogoGradRockCard}`
       };
     }
     else if(type === "steel") {
       return {
         card: `${typesClass.pokeTypeLogoSteelCard}`, 
         logo: `${typesClass.pokeTypeLogoSteel}`,
-        border: `${typesClass.pokeTypeLogoSteelCardBorder}`
+        border: `${typesClass.pokeTypeLogoSteelCardBorder}`,
+        gradCard: `${typesClass.pokeTypeLogoGradSteelCard}`
       };
     }
     else if(type === "ice") {
       return {
         card: `${typesClass.pokeTypeLogoIceCard}`, 
         logo: `${typesClass.pokeTypeLogoIce}`,
-        border: `${typesClass.pokeTypeLogoIceCardBorder}`
+        border: `${typesClass.pokeTypeLogoIceCardBorder}`,
+        gradCard: `${typesClass.pokeTypeLogoGradIceCard}`
       };
     }
     else if(type === "ghost") {
       return {
         card: `${typesClass.pokeTypeLogoGhostCard}`, 
         logo: `${typesClass.pokeTypeLogoGhost}`,
-        border: `${typesClass.pokeTypeLogoGhostCardBorder}`
+        border: `${typesClass.pokeTypeLogoGhostCardBorder}`,
+        gradCard: `${typesClass.pokeTypeLogoGradGhostCard}`
       };
     }
     else if(type === "dragon") {
       return {
         card: `${typesClass.pokeTypeLogoDragonCard}`, 
         logo: `${typesClass.pokeTypeLogoDragon}`,
-        border: `${typesClass.pokeTypeLogoDragonCardBorder}`
+        border: `${typesClass.pokeTypeLogoDragonCardBorder}`,
+        gradCard: `${typesClass.pokeTypeLogoGradDragonCard}`
       };
     }
     else if(type === "dark") {
       return {
         card: `${typesClass.pokeTypeLogoDarkCard}`, 
         logo: `${typesClass.pokeTypeLogoDark}`,
-        border: `${typesClass.pokeTypeLogoDarkCardBorder}`
+        border: `${typesClass.pokeTypeLogoDarkCardBorder}`,
+        gradCard: `${typesClass.pokeTypeLogoGradDarkCard}`
       };
     }
     return type;
