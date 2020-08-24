fffd5dd516d358239e43361cd4191ae633c53a33
Christian Garza <16683618+C-Garza@users.noreply.github.com>
2020-08-24 00:35:49 -0700

    Add width state and pass to NavDisplay. Fix background scroll when NavBar takes full screen

--- poke-app/src/components/NavBar/NavBar.js
+++ poke-app/src/components/NavBar/NavBar.js
@@ -6,32 +6,63 @@ import NavDisplay from "../NavDisplay/NavDisplay";
 const NavBar = () => {
   const [active, isActive] = useState(false);
   const [loaded, hasLoaded] = useState(false);
+  const [width, handleWidth] = useState(0);
 
   useEffect(() => {
     const handleEscKey = (e) => {
       if(e.keyCode === 27 && active) {
         isActive(!active);
       }
-    }
+    };
     window.addEventListener("keyup", handleEscKey);
     return () => {
       window.removeEventListener("keyup", handleEscKey);
     };
   }, [active]);
 
+  useEffect(() => {
+    const handleResize = (e) => {
+      handleWidth(window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth);
+    };
+    if(width === 0) {
+      handleWidth(window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth);
+    }
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [width]);
+
   const handleNavExpanded = () => {
     if(active && !loaded) {
       hasLoaded(!loaded); ////SECOND RENDER, FIX LATER
+      handleBodyScroll(true);
       return styles.nav__menu__expanded;
     }
     else if(active && loaded) {
+      handleBodyScroll(true);
       return styles.nav__menu__expanded;
     }
     else if(!active && loaded) {
+      handleBodyScroll(false);
       return styles.nav__menu__closed;
     }
     else {
       return "";
+    }
+  };
+  const handleBodyScroll = (isMenuOpen) => {
+    if(width > 600) {
+      if(document.body.style.overflow === "hidden") {
+        document.body.style.overflow = "auto";
+      }
+      return;
+    }
+    if(isMenuOpen) {
+      return document.body.style.overflow = "hidden";
+    }
+    else {
+      return document.body.style.overflow = "auto";
     }
   };
   const handleNavClick = () => {
@@ -65,7 +96,7 @@ const NavBar = () => {
         </div>
       </div>
       <div className={`${styles.nav__menu__list} ${active ? styles.nav__menu__list__expanded : ""}`}>
-        <NavDisplay isOpen={active} />
+        <NavDisplay isOpen={active} width={width} />
       </div>
     </nav>
   );
