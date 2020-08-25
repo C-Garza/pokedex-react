d3216385be1e27e0137d5232e3b2d0491744cc65
Christian Garza <16683618+C-Garza@users.noreply.github.com>
2020-08-25 03:00:22 -0700

    Remove uneeded import

--- poke-app/src/components/HomePage/HomePage.js
+++ poke-app/src/components/HomePage/HomePage.js
@@ -1,5 +1,4 @@
 import React from "react";
-import styles from "./HomePage.module.css";
 import PokeListContainer from "../PokeListContainer/PokeListContainer";
 
 class HomePage extends React.Component {
